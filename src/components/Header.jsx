276b7ce084550f5315ed01fd7a643dc7c981928b
alexkosogor <alexk-00@mail.ru>
2022-02-28 22:56:02 +0700

    test commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+// Можно сделать этот файл и с расширением .js
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://vsevsevmeste.ru/wp-content/uploads/2019/09/961-9610434_png-file-svg-google-chrome-logo-png.png" alt=""/>
+        </header>
+    );
+}
+
+export default Header
---
+++ src/components/ProfileComponent.jsx
@@ -0,0 +1,27 @@
+// Можно сделать этот файл и с расширением .js
+const ProfileComponent = () => {
+    return (
+        <div className="content">
+            <div className="panoramaImage">
+                <img src="http://darvidart.com.au/Gallery/photos/IMG_1309.jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProfileComponent
