41fab16b93a0904c1bf7d3ea49b0863b017e4be0
alexkosogor <alexk-00@mail.ru>
2022-03-01 17:54:19 +0700

    19-route-browserRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import ProfileComponent from "./components/Profile/ProfileComponent";
 import Dialogs from "./components/Dialogs/Dialogs"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 //test commit 2
 const App = () => {
   return (
-      <div className='app-wrapper'>
-          <Header />
-          <Navbar />
-          <div className='app-wrapper-content'>
-              {/*<ProfileComponent />*/}
-              <Dialogs />
+      <BrowserRouter>
+          <div className='app-wrapper'>
+              <Header />
+              <Navbar />
+              <div className='app-wrapper-content'>
+                  <Routes>
+                      <Route path="/profile" element={<ProfileComponent />}/>
+                      <Route path="/dialogs" element={<Dialogs />}/>
+                      <Route path="/news" element={<News />}/>
+                      <Route path="/music" element={<Music />}/>
+                      <Route path="/settings" element={<Settings />}/>
+                  </Routes>
+              </div>
           </div>
-      </div>
+      </BrowserRouter>
   );
 }
 
