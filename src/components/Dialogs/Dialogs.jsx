ad38f492a36b919caa7362a4e82b47016c9b9f64
alexkosogor <alexk-00@mail.ru>
2022-03-11 20:18:44 +0700

    31.2-sendMessageButtonWithOnClick

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,7 @@
 import s from "./Dialogs.module.css"
 import Message from "./Message/Message";
 import DialogItem from "./DialogItem/DialogItem";
+import React from "react";
 
 const Dialogs = (props) => {
 
@@ -11,6 +12,14 @@ const Dialogs = (props) => {
     let messagesElement = props.data.messages
         .map(msg => (<Message messageText={ msg.message } id={ msg.id } isMy={ msg.isMy }/>))  //Либо так
 
+    let newMessageElement = React.createRef()
+
+    let onAddMessageBtnClick = () => {
+        let messageText = newMessageElement.current.value
+        if(messageText.length <= 0) return
+        alert(messageText)
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -18,6 +27,14 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 { messagesElement }
+            </div>
+            <div className={s.dialogsItems}>
+                <div>
+                    <textarea ref={ newMessageElement } cols="30" rows="1"></textarea>
+                </div>
+                <div>
+                    <button onClick={ onAddMessageBtnClick }>Отправить сообщение</button>
+                </div>
             </div>
         </div>
     )
--- src/redux/state.js
+++ src/redux/state.js
@@ -32,6 +32,7 @@ let state = {
             {id:3, name:"Влад", img:"https://sun4-17.userapi.com/s/v1/if1/PI92XMRkl3N_ZOD0QBYFuo6NzJfUcwvTx8fM0yRh37RnOenBJ0mXq71RPDVRiUdgfBM-_c6m.jpg?size=50x50&quality=96&crop=298,3,2045,2045&ava=1"},
             {id:4, name:"Никита", img:"https://sun4-17.userapi.com/s/v1/if1/Cezd8dI-L8pjjgCXNE2g4X-2RBUkiwutcpfHI7HwGzkqSSlHTpVowoAKz6plstHPFup69QoX.jpg?size=50x50&quality=96&crop=0,187,1365,1365&ava=1"},
             {id:5, name:"Даня норм поц", img:"https://sun4-15.userapi.com/s/v1/ig2/S205i7auYN3Hvjvqc_VQtsMpmtIMsrVeYoen43lepENWJPjn2Il-YAPcNgJmZ6p0CmVixjvcXJtugS0e6lQEE9PQ.jpg?size=50x50&quality=96&crop=96,320,846,846&ava=1"},
+            {id:5, name:"Дёма", img:"https://sun4-10.userapi.com/s/v1/ig2/5kEwNhoj7s9s2fbQ7eoEAYGQ9MgvI2G5Bmhc9yOYm8MS27puLcQAfzQ0GFihTC6Eet5_kjPQgnoBEy6GRnSvvGGG.jpg?size=50x50&quality=96&crop=363,544,836,836&ava=1"},
         ]
     },
 }
