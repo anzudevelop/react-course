3f3984439ccd332ccf19c362bc34758c2ca65b1f
alexkosogor <alexk-00@mail.ru>
2022-05-17 21:08:36 +0700

    63-DataAccessLayer-axios.create

---
+++ src/api/api.js
@@ -0,0 +1,24 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': '39c617a6-5849-4d40-97e0-e73797f63d9f',
+    },
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 30) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+    },
+    followUnfollow(isFollowed, userId) {
+        if (!isFollowed) {
+            return instance.post(`follow/${userId}`)
+        }
+        else {
+            return instance.delete(`follow/${userId}`)
+        }
+    }
+}
+
